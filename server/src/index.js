9ee8de1dd3528f24eb06ffef63803a00bb912e0e
preetamf <preetamsinghe2202@gmail.com>
2024-04-28 14:38:51 +0530

    Database connect - setup

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import connectDB from "./db/dbconnection.js";
+import {app} from "./app.js" ;
+dotenv.config({
+    path: './.env'
+})
+
+//DataBase connection and App server start 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : http://localhost:${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
---
+++ server/src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            minlength: 2,
+            maxlength: 255,
+            required: true,
+            lowercase: true,
+            description: 'User\'s full name'
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            lowercase: true,
+            description: 'User\'s email address',
+            validate: {
+                validator: (v) => /\S+@\S+\.\S+/.test(v),
+                message: props => `${props.value} is not a valid email format!`
+            }
+        },
+        countryCode: {
+            type: String,
+            pattern: /^[A-Z]{2}$/,
+            required: true,
+            description: 'User\'s country code (ISO 3166-1 alpha-2)'
+        },
+        phoneNumber: {
+            type: String,
+            minlength: 9,
+            maxlength: 15,
+            required: true,
+            description: 'User\'s phone number (without special characters)'
+        },
+        password: {
+            type: String,
+            minlength: 8,
+            required: true,
+            writeOnly: true,
+            description: 'User\'s password (hashed and never stored in plain text)'
+        },
+        refreshToken: {
+            type: String,
+            readOnly: true,
+            description: 'User\'s refresh token (for access token renewal)'
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+//password hash
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) return next();
+
+        this.password = await bcrypt.hash(this.password, 10);
+        next()
+    } catch (error) {
+        console.log("error while password hashing: ", error)
+    }
+
+})
+
+//password validation after hashing
+userSchema.method.isPasswordCorrect = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password)
+    } catch (error) {
+        console.log("Password incorrect while compare: ", error)
+    }
+}
+
+//Generate token
+userSchema.method.generateAccessToken = function () {
+    return jwt.sign(
+        //payload
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET, //secret key
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema)
